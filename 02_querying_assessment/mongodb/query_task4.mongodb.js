503130b3cf8cac1492479cc9e999253add7f4d92
jetwat <jetwatwarathip@gmail.com>
2026-03-20 16:43:45 +0700

    submit MQL test 4

--- 02_querying_assessment/mongodb/query_task4.mongodb.js
+++ 02_querying_assessment/mongodb/query_task4.mongodb.js
@@ -17,5 +17,37 @@
 // are involved, and what MongoDB concepts you plan to use.
 // Write in English or Thai. Do not skip this step.
 //
+// ---------------------------------------------------------------
+// ---------------------------------------------------------------
 // Your thinking:
+// โจทย์ต้องการ แค่ total_revenue เท่านั้น ต้อง summary มา อาจต้องดูเพิ่มว่า trading period คือทั้งหมดหรือเปล่า
 //
+// #### สิ่งที่ต้องการ:
+// - ฟีลด์: total_revenue
+// - เงื่อนไข: ต้อง sum ทุก order
+//
+// #### บันทึกการทำงาน:
+// -> เข้าไปดู collection: orders *-- น่าจะไม่ต้องฟิลเตอร์วันที่ ต้องใช้แค่ fields: total_price*
+// -> เปิด docs หา ฟังก์ชัน sum *-- น่าจะต้องใช้ $group ใต้ .aggregate
+//
+// -> ลองเขียนและรัน
+//    ```
+//use("chrome-burger-db");
+// db.orders.aggregate(
+//   { $match: {} },
+//   { $group: { _id: 0, total_revenue: { $sum: "$total_price" } } },
+// );
+//    ``` *-- ผลลัพธ์น่าจะโอเค แต่จะลองใช้แค่ $group อย่างเดียว*
+//
+// -> ลองเขียนและรันใหม่
+//    ```
+use("chrome-burger-db");
+db.orders.aggregate(
+  {
+    $group: { _id: null, total_revenue: { $sum: "$total_price" } },
+  },
+  {
+    $project: { _id: 0, total_revenue: 1 },
+  },
+);
+//    ``` *-- ผลลัพธ์โอเค*
