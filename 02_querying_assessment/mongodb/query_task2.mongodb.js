4f1bee65499bcf10d42745dd97e046b9b2e23ff6
jetwat <jetwatwarathip@gmail.com>
2026-03-20 15:59:42 +0700

    submit MQL test 2

--- 02_querying_assessment/mongodb/query_task2.mongodb.js
+++ 02_querying_assessment/mongodb/query_task2.mongodb.js
@@ -17,5 +17,34 @@
 // are involved, and what MongoDB concepts you plan to use.
 // Write in English or Thai. Do not skip this step.
 //
+// ---------------------------------------------------------------
+// ---------------------------------------------------------------
 // Your thinking:
 //
+// โจทย์ต้องการ ดูออร์เดอร์ที่ Jane Doe รับผิดชอบ โดยต้องการเห็นแค่ เวลาที่ออเดอร์เกิดและคุณค่า เท่านั้น
+//
+// #### สิ่งที่ต้องการ:
+// - ฟีลด์: ออเดอร์เกิดและคุณค่า
+// - เงื่อนไข: ผู้รับผิดชอบ = "Jane Doe"
+//
+// #### บันทึกการทำงาน:
+// -> เปิด db ดู collection: orders ด้วย extension
+// -> น่าจะต้องใช้ .find ฟีลด์ order_date, total_price, staff{ first_name, last_name} จากคอลเล็คชัน orders แล้วโปรเจ็กต์แค่เท่าที่จำเป็น
+//
+// -> ลองเขียนและรัน
+//    ```
+// use("chrome-burger-db");
+// db.orders.find(
+//   { staff: { first_name: "Jane", last_name: "Doe" } },
+//   { _id: 0, order_date: 1, total_price: 1 },
+// );
+//    ``` *-- ผลลัพธ์ไม่โอเค ต้องเปลี่ยนเป็น staff.xxx*
+//
+// -> ลองเขียนและรันใหม่
+//    ```
+use("chrome-burger-db");
+db.orders.find(
+  { "staff.first_name": "Jane", "staff.last_name": "Doe" },
+  { _id: 0, order_date: 1, total_price: 1 },
+);
+//    ``` *-- ผลลัพธ์โอเค*
